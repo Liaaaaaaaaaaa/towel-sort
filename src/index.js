ed301756b13b95c3e7377d84f122b1d9db418fe5
Your NameLiaaaaaaaaaaagit config --global user.email you@example.comyllia_vladimirovna@mail.ru <you@example.com>
2023-01-23 10:27:06 +0300

    feat: add function towelSort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports =
+
+    // const matrix = [
+    //     [1, 2, 3, 4],
+    //     [5, 6, 7, 8],
+    //     [9, 10, 11, 12],
+    //     [13, 14, 15, 16],
+    // ];
+
+    //  matrix.map(function (it, index) { 
+
+    // console.log(matrix);
+
+
+function towelSort(matrix) {
+    if(matrix !== undefined && matrix.length > 0 ) {
+         matrix.forEach(function (element, index) {
+        if (index % 2 !== 0) { element.reverse() };
+
+    });
+
+    let matrixTotal = matrix.reduce((previousValue, item) => previousValue + ',' + item);
+    return matrixTotal.split(',').map(e => +e);
+    } else {
+        return [];
+    }
+   
+
 }
+
+//   console.log(towelSort(matrix));
+
